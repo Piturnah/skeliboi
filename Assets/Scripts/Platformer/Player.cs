01342bd95b2688c0580559f2134090700a5f7a3a
Piturnah <peterhebden6@gmail.com>
2020-04-19 11:38:44 +0100

    H/V Raycasting

---
+++ Assets/Scripts/Platformer/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    Controller2D controller;
+
+    private void Start() {
+        controller = GetComponent<Controller2D>();
+    }
+}
